8b8814444494c2e942f4da1b2fd01d89471f88be
oscar-rodriguez-lor22 <oscar.rodriguez.lor22@gmail.com>
2025-12-05 09:40:27 -0600

    updated laptop nvim config

--- Laptop/nvim/lua/plugins.lua
+++ Laptop/nvim/lua/plugins.lua
@@ -8,10 +8,12 @@ vim.cmd([[
   Plug 'norcalli/nvim-colorizer.lua'                    "Displays hex code colors when typed
   Plug 'folke/which-key.nvim'                           "Shortcut reference
   Plug 'nvim-telescope/telescope.nvim'                  "Modern fuzzy finder
-  Plug 'MeanderingProgrammer/render-markdown.nvim'       "Renders md files in buffer
+  Plug 'nvim-treesitter/nvim-treesitter'		"Better syntax highlighting
+  Plug 'MeanderingProgrammer/render-markdown.nvim'      "Renders md files in buffer
 
   " Fonts, Icons, Status Lines, & ColorScheme
   Plug 'nvim-tree/nvim-web-devicons'                    "Dev icons
+  Plug 'onsails/lspkind.nvim'				"Auto complete UI
   Plug 'windwp/nvim-autopairs'                          "Auto pairs
   Plug 'nvim-lualine/lualine.nvim'                      "Status line
   Plug 'folke/tokyonight.nvim'                  	"Colorscheme
@@ -27,6 +29,8 @@ vim.cmd([[
   Plug 'williamboman/mason-lspconfig.nvim'              "Mason nvim-lspconfig bridge
   Plug 'hrsh7th/cmp-nvim-lsp'                           "LSP nvim-cmp brige for autocomplete sugg>
   Plug 'hrsh7th/nvim-cmp'                               "Auto complete
+  Plug 'hrsh7th/cmp-buffer'      			" Buffer Source
+  Plug 'hrsh7th/cmp-path'        			" Path Source
 
   " Dependencies, not sure
   Plug 'MunifTanjim/nui.nvim'
@@ -47,8 +51,83 @@ require("neo-tree").setup({
 	},
 })
 
+require("mason").setup({
+    ensure_installed = {
+        "ts_ls",   
+        "pyright",    
+        "lua_ls",     
+        "html",       
+        "cssls",      
+    },
+})
+
+require("mason-lspconfig").setup({
+    automatic_installation = true, 
+    ensure_installed = {
+        "ts_ls",
+        "pyright",
+        "lua_ls",
+	"html",
+	"cssls",
+    },
+})
+
+require('nvim-treesitter.configs').setup({
+    highlight = {
+        enable = true,
+        disable = {},
+    },
+    ensure_installed = {
+	"c",
+	 "lua",
+	 "vim",
+	 "python", 
+	"javascript", 
+	"typescript", 
+	"tsx", 
+	"html", 
+	"css"
+	},
+    auto_install = true,
+})
+
+-- nvim-cmp config
+local cmp = require('cmp')
+local lspkind = require('lspkind')
+cmp.setup({
+    -- Sources for suggestions
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },  -- MUST be first to get LSP suggestions (errors, functions, etc.)
+        { name = 'buffer' },    -- Suggestions from the text in the current file
+        { name = 'path' },      -- File and directory paths
+    }),
+
+    -- Autocomplete menu looks
+    formatting = {
+        format = lspkind.cmp_format({
+            with_background = true,
+            menu = {
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                path = "[Path]",
+            },
+        }),
+    },
+
+    -- Keymaps for interacting with the completion menu
+    mapping = cmp.mapping.preset.insert({
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.select_next_item(),  
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(), 
+
+        -- To show the menu if it's closed
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }),
+})
+
+-- not working, fix
 require("tokyonight").setup({
-	style="day",
+	style="storm",
 	transparent = true,
 	terminal_colors = true,
 })
--- Laptop/nvim/lua/theme.lua
+++ Laptop/nvim/lua/theme.lua
@@ -1,7 +1,1 @@
-vim.cmd.colorscheme("tokyonight")
-
-if vim.api.nvim_get_option('colorscheme')  == 'tokyonight' then
-	print("Tokyo-Night Theme Applied")
-else
-	print("ERROR: Tokyo-Night THEME NOT APPLIED!")
-end
+vim.cmd[[colorscheme tokyonight-night]]
