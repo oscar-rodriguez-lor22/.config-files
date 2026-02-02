fc2910a5606928e5055ba7eb6827ac3de01ac243
oscar <oscar.rodriguez.lor22@gmail.com>
2026-02-02 11:44:22 -0600

    added some nvim fixes

--- NixOSLaptopConfig/configuration.nix
+++ NixOSLaptopConfig/configuration.nix
@@ -105,118 +105,67 @@
 	wget
 	ripgrep
 	fd
+	lua-language-server
+    pyright
+    nodePackages.typescript-language-server
+    vscode-langservers-extracted
 
 	(neovim.override {
-    		configure = {
-      			packages.myPlugins =  {
-        			start = [ vimPlugins.vim-plug ]; 
-      			};
-      			customRC = ''
-				source ${vimPlugins.vim-plug}/plug.vim
-        			call plug#begin('~/.local/share/nvim/plugged')
-          			Plug 'Mofiqul/adwaita.nvim'
-				Plug 'nvim-neo-tree/neo-tree.nvim'                    "File tree
-				Plug 'norcalli/nvim-colorizer.lua'                    "Displays hex code colors when typed
-  				Plug 'folke/which-key.nvim'                           "Shortcut reference
-  				Plug 'nvim-telescope/telescope.nvim'                  "Modern fuzzy finder
-  				Plug 'nvim-treesitter/nvim-treesitter'		"Better syntax highlighting
-  				Plug 'MeanderingProgrammer/render-markdown.nvim'      "Renders md files in buffer
+      configure = {
+        packages.myPlugins = with pkgs.vimPlugins; {
+          start = [ 
+            # UI & Essentials
+            adwaita-nvim neo-tree-nvim nvim-colorizer-lua which-key-nvim
+            telescope-nvim nvim-treesitter.withAllGrammars render-markdown-nvim
+            nvim-web-devicons lspkind-nvim nvim-autopairs lualine-nvim tokyonight-nvim
+            
+            # Dev & Git
+            vim-fugitive fterm-nvim
+            
+            # LSP & Autocomplete
+            nvim-lspconfig cmp-nvim-lsp nvim-cmp cmp-buffer cmp-path
+            nui-nvim plenary-nvim
+          ];
+        };
+        customRC = ''
+          lua << EOF
+          -- Basic UI
+          vim.opt.termguicolors = true
+          vim.opt.number = true
+          vim.opt.relativenumber = true
 
-  				" Fonts, Icons, Status Lines, & ColorScheme
-  				Plug 'nvim-tree/nvim-web-devicons'                    "Dev icons
-  				Plug 'onsails/lspkind.nvim'				"Auto complete UI
-  				Plug 'windwp/nvim-autopairs'                          "Auto pairs
-  				Plug 'nvim-lualine/lualine.nvim'                      "Status line
-  				Plug 'folke/tokyonight.nvim'                  	"Colorscheme
+          -- Neo-tree
+          require("neo-tree").setup({
+            filesystem = { filtered_items = { hide_dotfiles = false } }
+          })
 
-  				" Dev stuff
-  				Plug 'tpope/vim-fugitive'                             "Git integration
-  				Plug 'numToStr/FTerm.nvim'                            "Floating terminal
-  				Plug 'wolandark/vim-live-server',                     "Runs server
+          -- LSP Config (Directly calling binaries installed via Nix)
+          local lspconfig = require('lspconfig')
+          local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-  				" LSP
-  				Plug 'neovim/nvim-lspconfig'                          "Defines language server config
-  				Plug 'williamboman/mason.nvim'                        "Manages/installs LSP executables
-  				Plug 'williamboman/mason-lspconfig.nvim'              "Mason nvim-lspconfig bridge
-  				Plug 'hrsh7th/cmp-nvim-lsp'                           "LSP nvim-cmp brige for autocomplete sugg>
-  				Plug 'hrsh7th/nvim-cmp'                               "Auto complete
-  				Plug 'hrsh7th/cmp-buffer'      			" Buffer Source
-  				Plug 'hrsh7th/cmp-path'        			" Path Source
+          local servers = { 'ts_ls', 'pyright', 'lua_ls', 'html', 'cssls' }
+          for _, lsp in ipairs(servers) do
+            lspconfig[lsp].setup { capabilities = capabilities }
+          end
 
-  				" Dependencies, not sure
-  				Plug 'MunifTanjim/nui.nvim'
-  				Plug 'nvim-lua/plenary.nvim'
-        			call plug#end()
-
-				lua << EOF
-				require("neo-tree").setup({
-					filesystem = {
-						filtered_items = {
-							hide_dotfiles = false,
-		 					hide_gitignored = false,
-							visible = true,
-						},
-					},
-				})
-
-				require("mason").setup({
-    					ensure_installed = {
-        					"ts_ls",   
-        					"pyright",    
-        					"lua_ls",     
-        					"html",       
-        					"cssls",      
-    					},
-				})
-
-				require("mason-lspconfig").setup({
-    					automatic_installation = true, 
-    					ensure_installed = {
-        					"ts_ls",
-        					"pyright",
-        					"lua_ls",
-						"html",
-						"cssls",
-    					},
-				})
-
-				local cmp = require('cmp')
-				local lspkind = require('lspkind')
-				cmp.setup({
-    					-- Sources for suggestions
-    					sources = cmp.config.sources({
-        					{ name = 'nvim_lsp' },  -- MUST be first to get LSP suggestions (errors, functions, etc.)
-        					{ name = 'buffer' },    -- Suggestions from the text in the current file
-        					{ name = 'path' },      -- File and directory paths
-    					}),
-
-    				formatting = {
-        				format = lspkind.cmp_format({
-            					with_background = true,
-            					menu = {
-                					buffer = "[Buffer]",
-                					nvim_lsp = "[LSP]",
-                					path = "[Path]",
-            					},
-        				}),
-    				},
-
-    				mapping = cmp.mapping.preset.insert({
-        			['<CR>'] = cmp.mapping.confirm({ select = true }),
-        			['<Tab>'] = cmp.mapping.select_next_item(),  
-        			['<S-Tab>'] = cmp.mapping.select_prev_item(), 
-        			['<C-Space>'] = cmp.mapping.complete(),
-    			}),
-		})
-
-				vim.opt.termguicolors = true
-				vim.opt.number = true
-				vim.opt.relativenumber = true
-			EOF
-      			'';
-    		};
-	})
-	];
+          -- Autocomplete (CMP)
+          local cmp = require('cmp')
+          cmp.setup({
+            sources = {
+              { name = 'nvim_lsp' },
+              { name = 'buffer' },
+              { name = 'path' },
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<CR>'] = cmp.mapping.confirm({ select = true }),
+              ['<Tab>'] = cmp.mapping.select_next_item(),
+            }),
+          })
+          EOF
+		  '';
+      	};
+      })
+    ];
 	
 	environment.etc."xdg/kitty/kitty.conf".text = ''
 		background                #000000
